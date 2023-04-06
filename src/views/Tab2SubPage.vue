da7a7a7da94d2bd6a251a2762593cbad5191973a
Bayci <baycikplus@gmail.com>
2023-04-06 17:03:41 +0300

    back button added

--- src/views/Tab2SubPage.vue
+++ src/views/Tab2SubPage.vue
@@ -2,6 +2,9 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
+        <ion-buttons>
+          <ion-back-button/>
+        </ion-buttons>
         <ion-title>Tab 2</ion-title>
       </ion-toolbar>
     </ion-header>
@@ -18,5 +21,5 @@
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent,IonButtons,IonBackButton } from '@ionic/vue';
 </script>
